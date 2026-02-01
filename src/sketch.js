50a9861c54997119ba9ffd252cd25b2588b2a5fd
Nana Sarpong <ns8363@g.rit.edu>
2026-02-01 16:17:45 -0500

    Scoring Plan

--- src/sketch.js
+++ src/sketch.js
@@ -1,10 +1,25 @@
 //import shared session variable and current task variable
+/**
+ * STATION:Monster Counterpart 
+STOMACH: Torso 
+BRAINS: Arms 
+HEAD: Eyes 
+BLEEDING: Arms
+ */
 
-let stringSesh = "000"
+// Scoring Principles:
+// 0-5s : 45-50 points + multiplier (between 6.0 and 8.0 multiplier) - Good Monster Parts
+// 5-10s : 35 - 40 points + multiplier (between 3.0 and 5.0) - Medium Monster Parts
+// 10-14s : 30-35 points + multiplier (between 1.5 and 3.0) // Medium + Bad Monster Parts
+// 15s + : 25 points (no multiplier ) - Bad Monster Part
 
+
+let stringSesh = "000";
 
 let cycleDuration = 3000;
 let startTime;
+let timeLimit = 10; // 10 seconds
+let countDown;
 
 let bgImg;
 let idleVid;
@@ -13,30 +28,33 @@ let sessionData;
 
 let complete = false;
 
-let monsterTypes = ["cat", "demon", "mummy", "lava", "spider", "wolf"]
+let monsterTypes = ["cat", "demon", "mummy", "lava", "spider", "wolf"];
 
-let monsterVids = ["media/monsters/monster_1.mp4", 
+let monsterVids = [
+  "media/monsters/monster_1.mp4",
   "media/monsters/monster_2.mp4",
   "media/monsters/monster_3.mp4",
   "media/monsters/monster_4.mp4",
   "media/monsters/monster_5.mp4",
-  "media/monsters/monster_6.mp4"];
+  "media/monsters/monster_6.mp4",
+];
 
 function preload() {
   // Load JSON data
-  sessionData = loadJSON('sessions.json');
-  
-  bgImg = loadImage("media/background.png")
+  sessionData = loadJSON("sessions.json");
+
+  bgImg = loadImage("media/background.png");
 }
 
 function setup() {
   // Get the array of monsters for monster_1
-  sessionSet = sessionData[stringSesh]
+  sessionSet = sessionData[stringSesh];
 
-  let monsterType = monsterTypes[floor(random(monsterTypes.length))];/*sessionSet.monsterType*/
+  let monsterType =
+    monsterTypes[floor(random(monsterTypes.length))]; /*sessionSet.monsterType*/
 
   createCanvas(500, 900);
-  console.log(monsterType)
+  console.log(monsterType);
   taskVideo = createVideo(`./media/${monsterType}/${monsterType}.mp4`, () => {
     // make it autoplay-safe
     taskVideo.volume(0); // p5 wrapper volume
@@ -44,33 +62,40 @@ function setup() {
     taskVideo.elt.setAttribute("muted", "");
     taskVideo.elt.setAttribute("playsinline", ""); // iOS Safari inline playback
     taskVideo.loop(); // or .play()
+    // taskVideo.play()
     taskVideo.hide(); // we’ll draw it to the canvas
   });
   taskVideo.loop();
   taskVideo.hide();
 
-
   // Record start time
-  startTime = millis();
+  // startTime = millis();
 
-  imageMode(CORNER)
+  imageMode(CORNER);
 }
 
 function draw() {
-  if(complete){
-    
-  }
-  else {
+  startTime = int(millis() / 1000); // converted to seconds for testing
+  countDown = timeLimit - startTime; // countdown starts at 10
+  if (complete) {
+  } else {
     background(0);
     if (taskVideo) image(taskVideo, 0, 0, width, height);
   }
 
-  textSize(22);
-  fill('white');
-  text("Score: " + score, 300, 50)
+  if (countDown < 0) {
+    countDown = 0;
+  }
 
+  textSize(22);
+  fill("white");
+  text("Score: " + score, 300, 50);
+
+  textSize(22);
+  fill("white");
+  text("Timer: " + countDown, 300, 20);
 }
 
-function finalMonster() {
+function finalMonster() { 
 
 }
